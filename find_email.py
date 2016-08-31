7f88782a9564392775e63c2b88f50e8cef77b4af
redhat <redhat@localhost.localdomain>
2016-08-31 00:40:09 -0700

    input param

--- find_email.py
+++ find_email.py
@@ -1,7 +1,8 @@
+#coding=UTF-8
 import fileinput, re
 pat = re.compile(r'[a-z\-\.]+@[a-z\-\.]+', re.IGNORECASE)
 addresses = set()
-for line in fileinput.input():
+for line in fileinput.input("myemail.eml"): #input不传参数则需要在执行时传递文件名参数 
 	for address in pat.findall(line):
 		addresses.add(address)
 
