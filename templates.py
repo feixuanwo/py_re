ee18441e8c20bc32ee6cf0ae95db657ca9052852
redhat <redhat@localhost.localdomain>
2016-08-31 02:36:19 -0700

    eval exec

--- templates.py
+++ templates.py
@@ -1,9 +1,14 @@
+#coding=UTF-8
+#参数文件为test.txt
+
 import fileinput, re
-field_pat = re.compile(r'\[(.+?)]')
+import types
+field_pat = re.compile(r'\[(.+?)\]')
 
-scope = {}
-
+scope = {} #此处起到作用域的作用
+i=0
 def replacement(match):
+	#print type(match)
 	code = match.group(1)
 	try:
 		return str(eval(code, scope))
@@ -15,5 +20,7 @@ lines = []
 for line in fileinput.input():
 	lines.append(line)
 text = ''.join(lines)
+print text
+print type(text)
 
 print field_pat.sub(replacement, text)
